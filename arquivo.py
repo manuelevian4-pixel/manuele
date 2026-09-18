c17e65e7b113bc07a90d5803d796bc490bc52535
Manu Viana <manuelevian4@gmail.com>
2026-09-18 20:29:23 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,13 +1,7 @@
-def calcular_soma(numero1, numero2):
-    """
-    Retorna a soma de dois números.
-    """
-    return numero1 + numero2
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
 
+media = (nota1 + nota2 + nota3) / 3
 
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-
-resultado = calcular_soma(numero1, numero2)
-
-print("A soma é:", resultado)
+print(f"A média é: {media:.2f}")
